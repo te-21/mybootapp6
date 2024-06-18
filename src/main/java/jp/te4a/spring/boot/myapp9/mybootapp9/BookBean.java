fe588ca41ce4b7a901ed8630187961dfea04b416
te-21 <210773@jc-21.jp>
2024-06-18 11:07:16 +0900

    kadai9

--- src/main/java/jp/te4a/spring/boot/myapp9/mybootapp9/BookBean.java
+++ src/main/java/jp/te4a/spring/boot/myapp9/mybootapp9/BookBean.java
@@ -13,6 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
     public class BookBean {
+        @Id
         @GeneratedValue
         private Integer id ;
         @Column(nullable = false)
